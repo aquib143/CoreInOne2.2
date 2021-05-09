d0e576ff27cd737e3c46bd8b1718cbd6551ba889
Office <Office@SOLUTION10>
2021-05-10 04:13:05 +0530

    Database Changes

---
+++ CoreInOne2.2/Models/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreInOne2._2.Models
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options):base(options)
+        {
+
+        }
+    }
+}
